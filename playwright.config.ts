e2b43d8dd3baec737e14b1bf447025bedccff89b
DuyenNguyenNT <donguyenanh0105@gmail.com>
2026-08-20 00:18:42 +0700

    ci: Disable parallel execution to prevent test interference

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,12 +2,14 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-  fullyParallel: true,
+  /* Run tests in files in parallel */
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
+  workers: 1,
   reporter: [
     ['html'],
     ['allure-playwright']
